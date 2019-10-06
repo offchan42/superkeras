ab589fbf68a37e6d0f78adeb5f8d5a8d37c2443c
Chanchana Sornsoontorn <off9955555@gmail.com>
2019-10-06 12:50:25 +0700

    :memo: Add example code for many functions

--- datautil.py
+++ datautil.py
@@ -1,3 +1,7 @@
+"""
+tf.data utilities
+The most useful function that you can check is `create_xy_dataset_kit()`.
+"""
 import tensorflow as tf
 from tensorflow.data.experimental import AUTOTUNE
 from tensorflow.data import Dataset
@@ -20,7 +24,7 @@ class DatasetKit(
     )
 ):
     """
-    DatasetKit is supposed to contain [train data XOR test data], not both at the same time.
+    DatasetKit is supposed to contain either train or test set, but not both.
     You can pass `dsb` to kr.Model.fit() along with `steps`.
 
     # Attributes
@@ -51,6 +55,7 @@ def create_image_loader(
     The output tensor will consist of values between 0 and 1.
     The image will be resized if `width` and `height` are provided.
     The image must have a JPEG or PNG format.
+    See example code below.
 
     # Args
         channels: Number of color channels, e.g. 0 (automatically detected), 1, or 3
@@ -60,10 +65,24 @@ def create_image_loader(
             You must return the preprocessed uint8 image back to the caller.
             You need to convert tf.Tensor to a numpy array using `.numpy()`
             inside the function. (Eager execution must be enabled)
+            See example below.
         width: Target width of the image (after resizing)
         height: Target height of the image (after resizing)
-        resize_method: The interpolation method when resizing
-            (default=tf.image.ResizeMethod.AREA)
+        resize_method: The interpolation method when resizing.
+            default=tf.image.ResizeMethod.AREA (OpenCV said it's good for
+            downsampling images)
+
+    # Example
+        >>> import cv2 as cv
+        >>> def detect_edges(img):
+        >>>     img = img.numpy()
+        >>>     img = cv.Canny(img[...,0], 100, 200)
+        >>>     img = img[...,None]
+        >>>     return img
+        >>> image_loader = datautil.create_image_loader(channels=1, prep_func=detect_edges, width=target_w, height=target_h)
+        >>> img = image_loader('/path/to/image.jpg')  # a 3D tf.Tensor (target_h, target_w, 1)
+        >>> img = img.numpy()[...,0]  # eagerly convert to numpy to plot
+        >>> cv.imshow("img", img)
     """
     # if width and height are provided, we will resize the image
     hw_count = 0
@@ -77,7 +96,7 @@ def create_image_loader(
     def load_and_preprocess_image(path):
         """Load image from `path` and return as 3D tf.Tensor"""
         image = tf.io.read_file(path)
-        # cannot use decode_image() as it removes shape information
+        # cannot use decode_image() as it doesn't give shape information
         image = tf.cond(
             tf.image.is_jpeg(image),
             lambda: tf.image.decode_jpeg(image, channels=channels),
@@ -98,6 +117,7 @@ def create_image_loader(
 def create_xy_dataset(xs, ys, xmap=None):
     """
     Create a zipped dataset with (x, y) pairs when iterated through.
+
     # Args
         xs: A list of x, e.g. a list of strings representing image paths.
         ys: A list of y. e.g. a list of image labels (numpy array).
@@ -105,8 +125,19 @@ def create_xy_dataset(xs, ys, xmap=None):
         xmap: A function to map to each x in xs, to obtain new xs list.
             E.g. an image loader function which reads image from a path. It can
             be created by calling `create_image_loader(...)`.
+
     # Returns
         The zipped dataset and its number of samples
+
+    # Example
+        Creating dataset of images and labels
+        >>> paths = ['/path/img1.jpg', 'path/img2.jpg']
+        >>> labels = [[0.1,0.2,0.3], [0.4,0.5,0.6]]
+        >>> image_loader = datautil.create_image_loader(channels=3, width=100, height=200)
+        >>> ds, n = datautil.create_xy_dataset(paths, labels, xmap=image_loader)
+        >>> ds
+        <DatasetV1Adapter shapes: ((200, 100, 3), (3,)), types: (tf.float32, tf.float32)>
+        >>> assert n == 2
     """
     x_ds = Dataset.from_tensor_slices(xs)
     if xmap is not None:
@@ -121,15 +152,18 @@ def create_xy_dataset(xs, ys, xmap=None):
 def create_xy_dataset_kit(xy_dataset, n_samples, cache_path, shuffle, batch_size):
     """
     Create an (x,y) DatasetKit instance (you can check its doc for how to use it)
-    representing a training set or test set, but not both at the same time.
+    representing either a training set or test set, but not both at the same time.
+    It's designed to work nicely with Keras model.fit().
+    See the example code below.
 
     # Processing steps
         1. cache ds (maybe)
         2. dsb = ds
         3. shuffle dsb (maybe)
-        4. repeat dsb
+        4. repeat dsb (to work with keras.Model.fit(), it expects repeating dataset
+            for both train and validation set)
         5. batch dsb
-        6. prefetch dsb
+        6. prefetch dsb (to load next batch while the model is training)
 
     # Args
         xy_dataset: An instance of tf.data.Dataset created from `create_xy_dataset()`
@@ -141,9 +175,20 @@ def create_xy_dataset_kit(xy_dataset, n_samples, cache_path, shuffle, batch_size
             Choose `batch_size` that divides (or almost divides) `n_samples`
             will provide better evaluation measurement.
 
+    # Example
+        >>> ds, n = datautil.create_xy_dataset(...)
+        >>> train_kit = datautil.create_xy_dataset_kit(ds, n, None, True, 32)
+        >>> ds, n = datautil.create_xy_dataset(...)
+        >>> test_kit = datautil.create_xy_dataset_kit(ds, n, None, False, 256)
+        >>> model = keras.layers.Sequential(...)
+        >>> model.fit(train_kit.dsb, steps_per_epoch=train_kit.steps, epochs=200, validation_data=test_kit.dsb, validation_steps=test_kit.steps)
+        >>> loss, mae = model.evaluate(test_kit.ds.batch(256))
+
     # Troubleshooting
         If you see an error like `ValueError: Tensor's shape (x,) is not compatible
         with supplied shape (h, w, 1)` Make sure that you delete all cache files first.
+        Error can happen easily if the cache files are broken. So make sure to
+        delete them often.
     """
     ds = xy_dataset
     if cache_path is not None:
