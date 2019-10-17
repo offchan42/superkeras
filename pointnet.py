34ee430b9b83d0ad1ffe91bf8fe6dacc9ce75482
Chanchana Sornsoontorn <off9955555@gmail.com>
2019-10-18 00:48:42 +0700

    Make depth of PointNet adjustable

--- pointnet.py
+++ pointnet.py
@@ -12,25 +12,27 @@ kr = tf.keras
 kl = kr.layers
 
 
-def conv_stack(filters, kernel_size=1, batch_norm=True):
+def conv_stack(filters, kernel_size=1, batch_norm=True, depth=1.0):
     layers = []
     for f in filters:
+        f = int(round(f * depth))
         layers.append(kl.Conv1D(f, kernel_size, activation="relu"))
         if batch_norm:
             layers.append(kl.BatchNormalization())
     return LayerStack(layers)
 
 
-def dense_stack(units, batch_norm=True):
+def dense_stack(units, batch_norm=True, depth=1.0):
     layers = []
     for unit in units:
+        unit = int(round(unit * depth))
         layers.append(kl.Dense(unit, activation="relu"))
         if batch_norm:
             layers.append(kl.BatchNormalization())
     return LayerStack(layers)
 
 
-def build_tnet(input_dim, output_dim, n_points=None, name="t_net", **kwargs):
+def build_tnet(input_dim, output_dim, n_points=None, depth=1.0, name="t_net", **kwargs):
     """
     T-Net learns a transformation matrix to multiply to the points to obtain input
     permutation invariance. Look at figure 2 in the paper to see where it fits
@@ -40,13 +42,14 @@ def build_tnet(input_dim, output_dim, n_points=None, name="t_net", **kwargs):
     The output is shape (batch_size, output_dim, output_dim).
     """
     inp = kr.Input(shape=(n_points, input_dim))
-    x = conv_stack([64, 128, 1024])(inp)
+    x = conv_stack([64, 128, 1024], depth=depth)(inp)
     x = kl.GlobalMaxPooling1D()(x)
-    x = dense_stack([512, 256])(x)
+    x = dense_stack([512, 256], depth=depth)(x)
+    x_dims = x.shape[-1]
     output_dim_sqr = output_dim * output_dim
     x = kl.Dense(
         output_dim_sqr,
-        weights=[np.zeros([256, output_dim_sqr]), np.eye(output_dim).flatten()],
+        weights=[np.zeros([x_dims, output_dim_sqr]), np.eye(output_dim).flatten()],
     )(x)
     x = kl.Reshape((output_dim, output_dim))(x)
     model = kr.Model(inp, x, name=name, **kwargs)
@@ -75,12 +78,15 @@ def ExpandAndRepeat(dim, repeats):
     return kl.Lambda(expand, name="expand_and_repeat")
 
 
-def build_pointnet(n_dims, n_classes, n_points=None, mode="segmentation"):
+def build_pointnet(n_dims, n_classes, n_points=None, depth=1.0, mode="segmentation"):
     """Build the PointNet model that accept `n_points` inputs and output
     `n_points * n_classes` softmax value.
     # Args
         n_dims: Number of input dimensions e.g. 3 for 3 dimensional points
         n_classes: How many classes to predict for each point.
+        n_points: Number of input points and output points, can be None for
+            variable number of points.
+        depth: Multiplier to apply to the number of filters in each layer.
     The currently allowed mode is "segmentation" only. Classification will be
     supported in the future.
     """
@@ -89,14 +95,19 @@ def build_pointnet(n_dims, n_classes, n_points=None, mode="segmentation"):
 
     # the following operations mimic closely to figure 2 of the paper
     inp = kr.Input(shape=(n_points, n_dims))
-    input_tnet = build_tnet(n_dims, n_dims, n_points=n_points, name="input_tnet")
+    input_tnet = build_tnet(
+        n_dims, n_dims, n_points=n_points, depth=depth, name="input_tnet"
+    )
     tmp = transform(inp, input_tnet)  # input transform
-    tmp = conv_stack([64, 64])(tmp)  # shared MLP
-    feature_tnet = build_tnet(64, 64, n_points=n_points, name="feature_tnet")
+    tmp = conv_stack([64, 64], depth=depth)(tmp)  # shared MLP
+    tmp_dims = tmp.shape[-1]
+    feature_tnet = build_tnet(
+        tmp_dims, tmp_dims, n_points=n_points, depth=depth, name="feature_tnet"
+    )
     feature = transform(tmp, feature_tnet, name="feature")  # feature transform
 
-    # feature => global feature for classifier
-    tmp = conv_stack([64, 128, 1024])(feature)  # shared MLP
+    # convert feature to global feature (can be for classifier later)
+    tmp = conv_stack([64, 128, 1024], depth=depth)(feature)  # shared MLP
     global_feature = kl.GlobalMaxPooling1D()(tmp)
 
     # combine feature and global feature to make segmentation head
@@ -105,7 +116,7 @@ def build_pointnet(n_dims, n_classes, n_points=None, mode="segmentation"):
     combined_feature = kl.concatenate(
         [feature, global_feature_repeat], name="combined_feature"
     )
-    point_feature = conv_stack([512, 256, 128, 128])(combined_feature)
+    point_feature = conv_stack([512, 256, 128, 128], depth=depth)(combined_feature)
     out = kl.Conv1D(n_classes, 1, activation="softmax")(point_feature)
     model = kr.Model(inp, out, name="pointnet")
     return model
@@ -113,9 +124,8 @@ def build_pointnet(n_dims, n_classes, n_points=None, mode="segmentation"):
 
 if __name__ == "__main__":
     print(__doc__)
-    model = build_pointnet(3, 4, n_points=None)
+    model = build_pointnet(3, 4, n_points=10, depth=1.0)
     model.summary()
-    pred = model.predict(np.random.random((1, 500, 3)))
-    pp(pred.shape)
-    pp(pred)
-
+    pred = model.predict(np.random.random((1, 10, 3)))
+    print("pred.shape:", pred.shape)
+    print("pred[0]:\n", pred[0])
