91f00bad10aa8192fe9e6a1c19fd933d6923fbd4
Chanchana Sornsoontorn <off9955555@gmail.com>
2019-10-17 19:44:33 +0700

    Add input_dim and output_dim arguments

--- pointnet.py
+++ pointnet.py
@@ -11,7 +11,7 @@ kr = tf.keras
 kl = kr.layers
 
 
-def build_tnet(n_points=None, name="t_net", **kwargs):
+def build_tnet(input_dim, output_dim, n_points=None, name="t_net", **kwargs):
     """
     T-Net learns a transformation matrix to multiply to the points to obtain input
     permutation invariance. Look at figure 2 in the paper to see where it fits
@@ -19,10 +19,8 @@ def build_tnet(n_points=None, name="t_net", **kwargs):
 
     The input is shape (batch_size, n_points, input_dim).
     The output is shape (batch_size, output_dim, output_dim).
-    If `output_dim` is 0 or None, it will be set the same as `input_dim`.
-    `input_dim` will be inferred from the input tensor.
     """
-    inp = kr.Input(shape=(n_points, 3))
+    inp = kr.Input(shape=(n_points, input_dim))
     x = kl.Convolution1D(64, 1, activation="relu")(inp)
     x = kl.BatchNormalization()(x)
     x = kl.Convolution1D(128, 1, activation="relu")(x)
@@ -34,18 +32,19 @@ def build_tnet(n_points=None, name="t_net", **kwargs):
     x = kl.BatchNormalization()(x)
     x = kl.Dense(256, activation="relu")(x)
     x = kl.BatchNormalization()(x)
+    output_dim_sqr = output_dim * output_dim
     x = kl.Dense(
-        9,
-        weights=[
-            np.zeros([256, 9]),
-            np.array([1, 0, 0, 0, 1, 0, 0, 0, 1]).astype(np.float32),
-        ],
+        output_dim_sqr,
+        weights=[np.zeros([256, output_dim_sqr]), np.eye(output_dim).flatten()],
     )(x)
-    x = kl.Reshape((3, 3))(x)
-    return kr.Model(inp, x, name=name, **kwargs)
+    x = kl.Reshape((output_dim, output_dim))(x)
+    model = kr.Model(inp, x, name=name, **kwargs)
+    model.input_dim = input_dim
+    model.output_dim = output_dim
+    return model
 
 
 if __name__ == "__main__":
     print(__doc__)
-    tnet = build_tnet()
+    tnet = build_tnet(3, 3, n_points=1000)
     tnet.summary()
